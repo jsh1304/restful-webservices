31d34bcc592a2a2b11429d9d198ce08896cbcc7a
jsh1304 <jung3914a@gmail.com>
2024-03-26 11:27:15 +0900

    feat: 이름을 경로 변수로 전달하여 GET 요청
    -"/hello-world/path-variable/{name}" 엔드포인트에서 이름을 받아 HelloWorldBean 객체를 생성하여 반환

--- src/main/java/com/jj/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/jj/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.jj.rest.webservices.restfulwebservices;
 
 import com.jj.rest.webservices.restfulwebservices.helloworld.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 컨트롤러 클래스
@@ -15,5 +16,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean") // "/hello-world-bean" 경로에 대한 GET 요청을 처리
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World"); // "Hello World" 메시지를 가진 HelloWorldBean 객체를 생성하여 반환
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}") // "/hello-world/path-variable/{name}" 경로에 대한 GET 요청을 처리
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        // "Hello World {name}" 메시지를 가진 HelloWorldBean 객체를 생성하여 반환
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
